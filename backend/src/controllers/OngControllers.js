f9547c5cde410208ab742a0d56501a45c8a02a28
Aron <aronamaral@gmail.com>
2020-03-31 10:07:24 -0300

    recursos avançados

--- backend/src/controllers/OngControllers.js
+++ backend/src/controllers/OngControllers.js
@@ -1,5 +1,5 @@
-const crypto = require('crypto')
 const connection = require('../database/connection') 
+const generateBytesCode = require('../utils/generateUniqueId')
 
 module.exports = {
 
@@ -12,7 +12,7 @@ module.exports = {
     async create(request, response){
         const { name, email, whatsapp, city, uf } = request.body
     
-        const id = crypto.randomBytes(4).toString('HEX')
+        const id = generateBytesCode()
 
         await connection('ongs').insert({
             id,
